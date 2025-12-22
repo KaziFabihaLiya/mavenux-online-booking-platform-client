ebed43b5e1d3b50f5b47c62331866a04c38e341e
kazifabihaliya <kazifabihaliya@gmail.com>
2025-12-23 00:00:23 +0600

    URL Set

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import useAuth from "./useAuth";
 
 export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
   // baseURL: "/api",
   withCredentials: true,
 });
